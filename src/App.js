76f42453b26369bf3d61cc88a12be46dc243422b
Yennifer Campos Mora <yennifercamposmora@Yennifers-Air.fios-router.home>
2022-04-19 16:11:29 -0400

    Working

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import axios from 'axios';
-import {useEffect} from 'react';
-import Container from '@mui/material/Container';
-import Header from "./components/Header/Header";
-
+import Home from './pages/Home';
+import { AppRoutes } from './routes/AppRoutes';
 
 function App() {
   return (
     <div className="App">
-    <Container maxWidth="md" style={{display:"flex", flexDirection:"Column", height:'100vh'}}
-      >
-        <Header />
-      </Container>
+      <AppRoutes />
     </div>
   );
 }
 
 export default App;
+
